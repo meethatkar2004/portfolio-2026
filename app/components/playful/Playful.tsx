99295a9c8345d69430263874b20e1618dd15a7c9
meethatkar2004 <meethatkar2004@gmail.com>
2026-05-31 00:16:23 +0530

    feat: add CustomCursor, Playful UI, and Lanyard physics components

--- app/components/playful/Playful.tsx
+++ app/components/playful/Playful.tsx
@@ -8,7 +8,7 @@ const Playful = () => {
   const { isLoading } = useLoading();
   return (
     <div className='w-full h-screen overflow-hidden relative z-0'>
-      <div className='absolute w-full h-full inset-0 bg-linear-to-b from-transparent via-transparent to-[#343434] z-200'></div>
+      <div className='absolute w-full h-full inset-0 bg-linear-to-b from-[#343434] via-transparent to-[#343434] z-200'></div>
       <ImageTrail enabled={!isLoading} />
       <div className='h-full w-full flex items-center justify-center relative'>
         <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center gap-[2vmax] z-200'>
